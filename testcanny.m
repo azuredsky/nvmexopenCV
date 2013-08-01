4458345d60e27e55ad0f91c4d3f482dd8aeadf22
azuredsky <chfeilong@126.com>
2013-08-01 11:34:31 +0800

    new

--- testcanny.m
+++ testcanny.m
@@ -1,6 +1,6 @@
 %by sky.chen
 %generate canny.mexw
-nvcc('canny.cu')
+nvmex('canny.cu')
 %cannyCudaTest(fileName,thresholdLow,thresholdHigh,hysteresisIts,sigma,runs)
 %help edge
-cannyCudaTest('2.jpg',.05*1024,.1*1024,32,sqrt(2),10)
+cannyCudaTest('3.jpg',.05*1024,.1*1024,32,sqrt(2),10)
